c810e549d9475ad50cc52113e10d7c835bcef9f5
ZePandaSan <zepandasan@gmail.com>
2026-01-30 23:50:15 +0100

    Printing on tab format matchs.csv and joueurs.csv

--- scripts/app.py
+++ scripts/app.py
@@ -1,1 +1,17 @@
-print("Hello WC 2026 dashboard")
+from pathlib import Path
+import pandas as pd
+
+# Pour taper les fichiers peut importe où on se situe
+project_root = Path(__file__).parent.parent
+
+matchs_path = project_root / "data" / "matchs.csv"
+joueurs_path = project_root / "data" / "joueurs.csv"
+
+matchs = pd.read_csv(matchs_path)
+joueurs = pd.read_csv(joueurs_path)
+
+# Table print
+print("=== Matchs ===")
+print(matchs.head())
+print("\n=== Joueurs ===")
+print(joueurs.head())
