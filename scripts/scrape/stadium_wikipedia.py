4f2b4ffaea48fd7b309ba196fb28ff71a8a3c915
ZePandaSan <zepandasan@gmail.com>
2026-02-19 16:42:36 +0100

    using user-agent to bypass wikipedia anti-scrap

--- scripts/scrape/stadium_wikipedia.py
+++ scripts/scrape/stadium_wikipedia.py
@@ -1,7 +1,10 @@
 import requests
 
 url = "https://fr.wikipedia.org/wiki/Coupe_du_monde_de_football_2026#Stades"
-response = requests.get(url)
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+response = requests.get(url, headers=headers)
 html_content = response.text
 
 print(html_content)
