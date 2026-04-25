78fe578141948072cb7b425dc5d3ca3e07e3f431
ZePandaSan <zepandasan@gmail.com>
2026-04-25 14:50:28 +0200

    feat: column ingestion_timestamp insteed ingestion_data + source_file_name

--- scripts/transform.py
+++ scripts/transform.py
@@ -10,9 +10,10 @@ PROCESSED_DIR = "data/processed"
 # Retrieve today date  
 today = datetime.now().strftime("%Y%m%d")
 ingestion_date = datetime.now().strftime("%Y-%m-%d")
+ingestion_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 input_file = os.path.join(RAW_DIR, f"baseWC_{today}.json")
-
+source_file_name = os.path.basename(input_file)
 
 output_file = os.path.join(PROCESSED_DIR, "matches.csv")
 
@@ -30,15 +31,15 @@ matches = source.get("matches", [])
 rows = []
 for match in matches:
     row = {
+        "ingestion_date": ingestion_date,
+        "source_file_name": source_file_name,
         "date": match.get("date"),
         "time": match.get("time"),
         "team1": match.get("team1"),
         "team2": match.get("team2"),
         "group": match.get("group"),
         "ground": match.get("ground"),
-        "round": match.get("round"),
-        "ingestion_date": ingestion_date
-        
+        "round": match.get("round")
     }
     rows.append(row)
 
